b6c08a135629877dd7bca127f2c765bbec30abc6
SIBSANKAR RANA <isibsankarrana@gmail.com>
2023-02-09 13:04:54 +0530

    add prompt massage

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-alert("Hello! I'm alert");
+//alert("Hello! I'm alert");
+//input
+var username = prompt("Give me username")
 
+//processing
+var welcomeMessage= "this Script works! " + username;
+
+//output
+
+alert(welcomeMessage); 
