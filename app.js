4b20249aa0781880fff5a63980626c21b6678a73
SIBSANKAR RANA <isibsankarrana@gmail.com>
2023-02-10 21:47:43 +0530

    Translation done

--- app.js
+++ app.js
@@ -13,12 +13,30 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input");
 var outputDiv= document.querySelector("#output");
 
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+ var serverURL ="https://api.funtranslations.com/translate/minion.json"
 
+function getTranslationURL(text){
+ return serverURL + "?" + "text=" +text
+} 
+
+function errorHandler(error){
+        consol.log("error occured", error);
+        
+}
 function clickHandler(){
     
         
-     outputDiv.innerText="asdfgfdsawerfg "+ textInput.value;
-        
+     //outputDiv.innerText="asdfgfdsawerfg "+ textInput.value;
+     var inputText = textInput.value;
+
+     fetch(getTranslationURL(inputText))
+     .then(response => response.json())
+     .then(json => {
+        var translateText = json.contents.translated;
+        outputDiv.innerText = translateText;
+     })
+     .catch(errorHandler)   
 };
 
 btnTranslate.addEventListener("click", clickHandler)
